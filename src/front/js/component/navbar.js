0fb9bbe36de61cde583435caa6c952be852a50b8
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-10-05 16:10:58 +0000

    primer cambio

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import bootstrap from "bootstrap";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
+		<div className="navbar">
+		<nav class="navbar navbar-light bg-light justify-content-between">
+			<a class="navbar-brand">Navbar</a>
+			<form class="form-inline">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></input>
+			</form>
 		</nav>
+		</div>
 	);
 };
